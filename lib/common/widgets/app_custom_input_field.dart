b77533dc64f3989ce5b50907cda7faf67b864290
mdalraihan452 <mdalraihan452@gmail.com>
2026-05-13 13:14:59 +0600

    Polish create user header, gender dropdown, and input text styling

---
+++ lib/common/widgets/app_custom_input_field.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+import '../theme/app_colors.dart';
+import '../theme/app_palette.dart';
+import '../theme/app_spacing.dart';
+import '../theme/app_text_styles.dart';
+
+class AppCustomInputField extends StatelessWidget {
+  const AppCustomInputField({
+    super.key,
+    required this.hintText,
+    this.controller,
+    this.keyboardType,
+    this.obscureText = false,
+    this.readOnly = false,
+    this.suffixIcon,
+    this.onChanged,
+    this.onTap,
+  });
+
+  final String hintText;
+  final TextEditingController? controller;
+  final TextInputType? keyboardType;
+  final bool obscureText;
+  final bool readOnly;
+  final Widget? suffixIcon;
+  final ValueChanged<String>? onChanged;
+  final VoidCallback? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all(color: AppPalette.borderSoftBlue),
+        borderRadius: BorderRadius.circular(18),
+        boxShadow: AppPalette.softShadow,
+      ),
+      padding: const EdgeInsets.fromLTRB(
+        AppSpacing.md,
+        AppSpacing.xs,
+        AppSpacing.xs,
+        AppSpacing.xs,
+      ),
+      child: TextField(
+        controller: controller,
+        keyboardType: keyboardType,
+        obscureText: obscureText,
+        readOnly: readOnly,
+        style: AppTextStyles.body2.copyWith(color: AppColors.textSecondary),
+        onChanged: onChanged,
+        onTap: onTap,
+        decoration: InputDecoration(
+          hintText: hintText,
+          hintStyle: AppTextStyles.body2.copyWith(
+            color: AppColors.textSecondary,
+          ),
+          fillColor: Colors.white,
+          filled: true,
+          border: InputBorder.none,
+          enabledBorder: InputBorder.none,
+          focusedBorder: InputBorder.none,
+          suffixIcon: suffixIcon,
+        ),
+      ),
+    );
+  }
+}
